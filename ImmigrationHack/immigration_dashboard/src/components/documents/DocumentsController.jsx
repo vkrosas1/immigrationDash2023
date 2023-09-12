4e32d782106fe3aa426a7a2786b9788050dfb9ae
vkrosas1 <vivianarosas@microsoft.com>
2023-09-12 09:44:32 -0700

    current wip

--- ImmigrationHack/immigration_dashboard/src/components/documents/DocumentsController.jsx
+++ ImmigrationHack/immigration_dashboard/src/components/documents/DocumentsController.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import FileService from "../../api/FileService";
 import { STATUSES, TEMP_USER } from "../../Contstants";
+import DocDropdown from "./DocDropdown";
 
 function DocumentView(props) {
 
@@ -35,26 +36,65 @@ function DocumentView(props) {
         props.setIsModalOpen(false);
     }
 
+    const documentOptions = [
+        { value: "birth certificate", label: "Birth Certificate" },
+        { value: "high school diploma", label: "High School Diploma" },
+        { value: "permanent residency card", label: "Permanent Residency Card" },
+        { value: "n-400", label: "N-400" },
+        { value: "marriage license", label: "Marriage License" },
+        { value: "passport", label: "Passport" },
+        { value: "g1450", label: "G-1450" },
+        { value: "g28", label: "G-28" }
+    ];
+
     return (
-        <div class="is-flex is-flex-direction-column">
-            <div class="columns">
-                <div class="column is-one-third">
-                    <figure class="image is-2by3">
-                        <img src={imgSrc} alt="File Preview" />
-                    </figure>
-                    <input type='file' name='Select File' onChange={onChange} />
-                    <button class="button " onClick={onUpload}>Upload</button>
-                </div>
-                <div class="column">
-                    <h1 class="is-underlined is-size-3">{props.selectedDocumentForm.id}</h1>
-                    <p class="is-size-5">{props.selectedDocumentForm.data}</p>
-                </div>
-            </div>
-            <div class="is-flex is-justify-content-end is-align-self-flex-end">
-                <button class="button" onClick={onClose}>Close</button>
-            </div>
+        <div className="App">
+           
+            <form action="/my-handling-form-page" method="post">
+                <DocDropdown
+                    isSearchable
+                    isMulti
+                    placeHolder="Select..."
+                    options={documentOptions}
+                    onChange={(value) => console.log(value)}
+                />
+                <ul>
+
+                    <li>
+                        <label for="msg">Comments</label>
+                        <textarea id="msg" name="user_message"></textarea>
+                    </li>
+
+                    <button type="submit">Upload</button>
+                </ul>
+            </form>
+
         </div>
     );
+    // create ui that will give a drop down of possible forms to upload and allows the user to input the name of the form
+    // and then upload the file to the server
+
+    /*
+        return (
+            <div class="is-flex is-flex-direction-column">
+                <div class="columns">
+                    <div class="column is-one-third">
+                        <figure class="image is-2by3">
+                            <img src={imgSrc} alt="File Preview" />
+                        </figure>
+                        <input type='file' name='Select File' onChange={onChange} />
+                        <button class="button " onClick={onUpload}>Upload</button>
+                    </div>
+                  {*//*  <div class="column">
+<h1 class="is-underlined is-size-3">{props.selectedDocumentForm.id}</h1>
+<p class="is-size-5">{props.selectedDocumentForm.data}</p>
+</div>*//*}
+</div>
+<div class="is-flex is-justify-content-end is-align-self-flex-end">
+  <button class="button" onClick={onClose}>Close</button>
+</div>
+</div>
+);*/
 }
 
 export default DocumentView;
