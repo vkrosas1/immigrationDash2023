2cb1cc2ea92227a6bf845f6091c50421d3c50e18
Rida Shahid <ridashahid@microsoft.com>
2023-09-12 15:09:59 -0700

    fix user errors

--- ImmigrationHack/immigration_dashboard/src/components/home/HomeController.jsx
+++ ImmigrationHack/immigration_dashboard/src/components/home/HomeController.jsx
@@ -8,6 +8,12 @@ function HomeController(props) {
     const [errorMessages, setErrorMessages] = useState({});
     const [isSubmitted, setIsSubmitted] = useState(false);
     const [isLogIn, setIsLogIn] = useState(false);
+    const [fname, setFName] = useState(""); 
+    const [email, setEmail] = useState(""); 
+    const [lname, setLName] = useState(""); 
+    const [pass, setPass] = useState(""); 
+    const [repass, setRepass] = useState(""); 
+    const [country, setCountry] = useState(""); 
 
     const errors = {
         uname: "invalid username or password",
@@ -18,15 +24,14 @@ function HomeController(props) {
         //Prevent page reload
         event.preventDefault();
 
-        var { uname, pass, repass, fname, lname, country } = document.forms[0];
 
         if (isLogIn) {
             // Find user login info
-            const userData = UserService.authenticateUser(uname, pass);
+            const userData = UserService.authenticateUser(email, pass);
 
             // Compare user info
             if (userData) {
-                UserService.getUserInfo(uname);
+                UserService.getUserInfo(email);
             }
             else {
                 // Username or password incorrect
@@ -38,7 +43,7 @@ function HomeController(props) {
                 setErrorMessages({ name: "repass", message: errors.repass });
             }
             else {
-                UserService.createUser(uname, pass, fname, lname, country);
+                UserService.createUser(email, pass, fname, lname, country);
             }
         }
     };
@@ -54,17 +59,17 @@ function HomeController(props) {
         <div className="container-login">
             <div style={{ transform: `translate(${isLogIn ? 0 : 300}px, 0px)` }} className="form-div">
                 <form onSubmit={handleSubmit}>
-                    <div> <input placeholder="Email Address" type="text" name="uname" required />
+                    <div> <input placeholder="Email Address" type="text" value={email} onChange={(e) => setEmail(e.target.value)} required />
                         {renderErrorMessage("uname")}
                     </div>
-                    <div> <input placeholder="Password" type="password" name="pass" required />
+                    <div> <input placeholder="Password" type="password" value={pass} onChange={(e) => setPass(e.target.value)} required />
                         {renderErrorMessage("pass")}
                     </div>
                     <div> {isLogIn ? '' :
-                        <div><input placeholder="Re-enter password" type="password" name="repass" required />
-                            <input placeholder="First Name" type="text" name="fname" required />
-                            <input placeholder="Last Name" type="text" name="lname" required />
-                            <input placeholder="Country of Citizenship" type="text" name="country" required />   </div>  }
+                        <div><input placeholder="Re-enter password" type="password" value={repass} onChange={(e) => setRepass(e.target.value)} required />
+                            <input placeholder="First Name" type="text" value={fname} onChange={(e) => setFName(e.target.value)} required />
+                            <input placeholder="Last Name" type="text" value={lname} onChange={(e) => setLName(e.target.value)} required />
+                            <input placeholder="Country of Citizenship" type="text" value={country} onChange={(e) => setCountry(e.target.value)} required />   </div>  }
                         {renderErrorMessage("repass")}</div>
                         <button className="button-primary">Submit</button>
                 </form>
