2aa8e12749d30a9237cd2b5373166b001fa6792e
Deepak Verma <deepakverma@microsoft.com>
2023-09-12 01:07:45 -0700

    dbrelated changes

---
+++ ImmigrationHack/Services/src/Data/ApplicationDbContenxt.cs
@@ -0,0 +1,31 @@
+﻿namespace ImmigrationHack.Services.src.Data
+{
+    using ImmigrationHack.Services.src.Data.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _config;
+        private readonly ILogger<ApplicationDbContext> _logger;
+
+        public ApplicationDbContext(IConfiguration config, ILogger<ApplicationDbContext> logger)
+        {
+            _config = config;
+            _logger = logger;
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserDocument> UserDocuments { get; set; }
+        public DbSet<Path> Paths { get; set; }
+        public DbSet<Form> Forms { get; set; }
+        public DbSet<DocumentType> DocumentTypes { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = _config.GetConnectionString("Database");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        //public DbSet<YourEntity> YourEntities { get; set; }
+    }
+
+}
---
+++ ImmigrationHack/Services/src/Repository/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using ImmigrationHack.Services.src.Data.Entities;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace ImmigrationHack.Services.src.Repository
+{
+    public interface IUserRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Attach<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        IQueryable<T> GetAll<T>() where T : class;
+        ValueTask<T?> GetAsync<T>(Guid id) where T : class;
+        EntityEntry<T> GetEntry<T>(T entity) where T : class;
+        User? GetUserByKey(Guid? userId);
+        User? GetUserByEmail(string? emailId);
+        List<UserDocument>? GetUserDocumentsByuserId(Guid userId);
+        Task<bool> SaveChangesAsync();
+        void Update<T>(T entity) where T : class;
+    }
+}
