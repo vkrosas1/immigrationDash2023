2923df5a3310c215ae822ee6c43996cf18a349e1
Rida Shahid <ridashahid@microsoft.com>
2023-09-14 15:47:21 -0700

    doc fix + some error handling

--- ImmigrationHack/immigration_dashboard/src/components/usercontext/UserContext.jsx
+++ ImmigrationHack/immigration_dashboard/src/components/usercontext/UserContext.jsx
@@ -1,31 +1,19 @@
 // UserContext.js
 import { createContext, useContext, useState } from 'react';
 
-export const UserContext = createContext();
-/*const UserContextEmail = createContext();
-*/
+const UserContext = createContext();
+
 export const useUser = () => {
     return useContext(UserContext);
 };
 
-export const UserProvider = props => {
+export const UserProvider = ({ children }) => {
     const [userResult, setUserResult] = useState(null);
-    const [userEmail, setUserEmail] = useState(null);
 
     return (
-        <UserContext.Provider
-            value={{ value: [userResult, setUserResult], value2: [userEmail, setUserEmail] }}
-        >
-            {props.children}
+        <UserContext.Provider value={{ userResult, setUserResult }}>
+            {children}
         </UserContext.Provider>
     );
 };
 
-/*export const UserProviderEmail = ({ children }) => {
-    const[userEmail, setUserEmail] = useState(null);
-    return (
-        <UserContext.Provider value={{ userEmail, setUserEmail }}>
-            {children}
-        </UserContext.Provider>
-    );
-};*/
