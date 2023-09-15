640fe2cb03e141930df12831d5095f3f82c4ad8c
vkrosas1 <vivianarosas@microsoft.com>
2023-09-14 17:07:40 -0700

    path show

--- ImmigrationHack/immigration_dashboard/src/components/home/HomeController.jsx
+++ ImmigrationHack/immigration_dashboard/src/components/home/HomeController.jsx
@@ -1,18 +1,29 @@
 /*import TreeView from "./TreeView";
 */
+import { useEffect } from "react";
 import PathService from "../../api/PathService";
 import UserService from "../../api/UserService";
 import React, { useState } from "react";
 
 function HomeController(props) {
     // React States
+    const [validPaths, setValidPaths] = useState([]);
+    useEffect(() => {
+        const getPaths = async () => {
+            const getUserInfo = await UserService.getUserInfo(localStorage.getItem('email'));
+            console.log(getUserInfo);
+            const paths = await PathService.getEligiblePaths(getUserInfo.data.id);
+            console.log("got here");
+            setValidPaths(paths.data[0]);
+            if (paths.data[0]) {
+                return "yay got paths";
 
-    const getPaths = async () => {
-        const getUserInfo = await UserService.getUserInfo(localStorage.getItem('email'));
-        console.log(getUserInfo);
-        const paths = await PathService.getEligiblePaths(getUserInfo.data.id)
-        return paths; 
-    }
+            }
+            return "You haven't added any documents yet";
+        }; getPaths();
+    }, []);
+
+   
    /* const getDisplay = () => {
         if (!selectedValue || selectedValue.length === 0 || props.hasBeenSubmitted) {
             return props.placeHolder;
@@ -25,7 +36,7 @@ function HomeController(props) {
     return (
         <div className="app">
             <div className="login-form">
-                <div className="title">{getPaths()}</div>
+                <div className="title">{validPaths}</div>
                 
             </div>
         </div>
